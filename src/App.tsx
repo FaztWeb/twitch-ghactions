06dc64564c65095929e0ebc0f4ac7024b1e50a27
Fazt <fazt@faztweb.com>
2022-08-14 04:53:50 +0200

    click añadido

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Button from '@mui/material/Button';
 function App() {
   return (
     <div>
-      <Button variant="contained">Hola Mundox!</Button>
+      <Button variant="contained" onClick={() => {
+        alert('click!')
+      }}>Click</Button>
     </div>
   )
 }
