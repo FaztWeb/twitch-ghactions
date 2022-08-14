0fc89319cf1df372d721e25839cf592b015600db
Fazt <fazt@faztweb.com>
2022-08-14 03:58:07 +0200

    text hola mundo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 function App() {
   return (
     <div>
-      <Button variant="contained">Hello World</Button>
+      <Button variant="contained">Hola Mundo</Button>
     </div>
   )
 }
